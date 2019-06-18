8c18dacf9f02fb09fbaa453c415c5afca75e6041
unknown <shebkoch@gmail.com>
2019-06-18 19:13:30 +0300

    fix venv

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\shebk\PycharmProjects\sec\venv"
+$env:VIRTUAL_ENV="D:\Projects\Code-to-Doc\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
